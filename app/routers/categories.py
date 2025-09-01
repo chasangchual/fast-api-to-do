812d0d1b14afc886e6d70d510ba2396e8c237881
Sangchual Cha <scha@commonwealthretirement.com>
2025-08-31 23:50:58 -0400

    Add a new endpoint, GET /categories/{public_id}, to find a category with given id

--- app/routers/categories.py
+++ app/routers/categories.py
@@ -1,5 +1,7 @@
 from typing import Annotated, List
-from fastapi import APIRouter, Depends
+from uuid import UUID
+
+from fastapi import APIRouter, HTTPException
 from sqlalchemy.orm import Session
 from starlette import status
 from app.models.todo import Category
@@ -10,8 +12,14 @@ categories_router = APIRouter(
     prefix="/categories"
 )
 
-@categories_router.get("/", status_code=status.HTTP_200_OK)
+@categories_router.get("", status_code=status.HTTP_200_OK)
 def get_all_categories(db: db_dependency) -> List[CategoryResponse]:
     categories = db.query(Category).all()
     return [CategoryResponse(category) for category in categories]
 
+@categories_router.get("/{public_id}", status_code=status.HTTP_200_OK)
+def get_category_by_id(public_id: UUID, db: db_dependency) -> CategoryResponse:
+    category = db.query(Category).filter(Category.public_id == public_id).first()
+    if category is not None:
+        return CategoryResponse(category)
+    raise HTTPException(status_code=404, detail=f"Category with id [{public_id}] not found")
